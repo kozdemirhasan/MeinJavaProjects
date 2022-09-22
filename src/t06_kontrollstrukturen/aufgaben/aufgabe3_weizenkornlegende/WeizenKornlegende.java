dbd233caf08dcc93682509dbdef8d93c82938278
Hasan Murat Kücüközdemir <kozdemirhasan@gmail.com>
2022-09-22 22:49:19 +0200

    WeisenKornlegende 2

--- src/t06_kontrollstrukturen/aufgaben/aufgabe3_weizenkornlegende/WeizenKornlegende.java
+++ src/t06_kontrollstrukturen/aufgaben/aufgabe3_weizenkornlegende/WeizenKornlegende.java
@@ -4,15 +4,15 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class WeizenKornlegende {
     public static void main(String[] args) {
-        long summe=1;
-        long box=1;
-        for (int i=2; i<64;i++){
+        double summe=0;
+        double box=0.5;
+        for (int i=1; i<65;i++){
             box= box*2;
             summe = summe +box;
-            System.out.print("box: " + box);
-            System.out.println(",  summe: " + summe);
+            System.out.print(i+ " => box: " + (long) box);
+            System.out.println(",  summe: " + (long) summe);
         }
 
-        System.out.println(summe*0.05/1000);
+        System.out.println("\nWeight: " + summe*0.05/1000 + " Kg");
     }
 }
